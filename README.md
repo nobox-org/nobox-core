efb3d4da862d291b04c33b050efb2b5da307d58b
Akintunde Jegede <akintunde.jegede@primarybid.com>
2022-07-15 07:49:12 +0100

    - updated Readme - updated setup - added grapqhiql json

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 - To installation: `npm install`
 - To setup docker: `npm run setup`
 - To run nobox: `npm run start:debug`
+
+
+# Export Graphiql Playground JSON
+- Install [this chrome extension](https://chrome.google.com/webstore/detail/localstorage-manager/fkhoimdhngkiicbjobkinobjkoefhkap/related
+)
+- Navigate to [playground](http://localhost:8000/_internal_/graphql)
+- Click the Installed Extension
+- And import the contents of `_/graphiql.json`
+- Reload [playground](http://localhost:8000/_internal_/graphql)
--- setup.sh
+++ setup.sh
@@ -3,9 +3,10 @@
 echo "Setting up Mongo"
 cd docker
 mongo_container="n_mongo"
+mongo_image="mongo"
 if docker ps | awk -v app="$mongo_container" 'NR > 1 && $NF == app{ret=1; exit} END{exit !ret}'; then
   docker stop "$mongo_container" && docker rm -f "$mongo_container"
   echo "Deleting old Mongo container"
 fi
-docker run --name "$mongo_container" -d -p 27017:27017 --env-file ../env/.local.env "$mongo_container"
+docker run --name "$mongo_container" -d -p 27017:27017 --env-file ../env/.local.env mongo "$mongo_image"
 cd ..
