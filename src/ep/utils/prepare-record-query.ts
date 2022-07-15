5abe880f217f0708214ef321150a0f270a71a146
Akintunde Jegede <akintunde.jegede@primarybid.com>
2022-07-15 08:38:36 +0100

    added id-based query updated package(-lock).json

--- src/ep/utils/prepare-record-query.ts
+++ src/ep/utils/prepare-record-query.ts
@@ -1,12 +1,12 @@
 import { RecordStructureType } from "@/record-spaces/dto/record-structure-type.enum";
 import { RecordField, Record as Record_ } from "@/schemas";
 import { throwBadRequest } from "@/utils/exceptions";
-import { FilterQuery } from "mongoose";
+import mongoose, { FilterQuery } from "mongoose";
 import { getQueryFieldDetails } from "./get-query-field-details";
 
-export const prepareRecordQuery = (recordSpaceSlug: string,recordSpaceId: string, query: Record<string, string>, fieldsDetailsFromDb: RecordField[], logger: any) => {
-    const queryKeys = Object.keys(query);
-    const mongoQuery$And = [];
+export const prepareRecordQuery = (recordSpaceSlug: string, recordSpaceId: string, query: Record<string, string>, fieldsDetailsFromDb: RecordField[], logger: any) => {
+    const { queryKeys, preparedQuery } = initPreparedQuery(recordSpaceId, query);
+
     for (let index = 0; index < queryKeys.length; index++) {
         const queryKey = queryKeys[index];
         const fieldDetails = getQueryFieldDetails(queryKey.toLowerCase(), fieldsDetailsFromDb);
@@ -18,11 +18,28 @@ export const prepareRecordQuery = (recordSpaceSlug: string,recordSpaceId: string
             [RecordStructureType.TEXT]: "textContent",
             [RecordStructureType.NUMBER]: "numberContent",
         }[type];
-        mongoQuery$And.push({ 'fieldsContent.field': _id, [`fieldsContent.${valueType}`]: query[queryKey] });
-    }
-    const preparedQuery: FilterQuery<Record_> = {
-        recordSpace: recordSpaceId,
-        $and: mongoQuery$And
+        preparedQuery.$and.push({ 'fieldsContent.field': _id, [`fieldsContent.${valueType}`]: query[queryKey] });
     }
     return preparedQuery;
+}
+
+const initPreparedQuery = (recordSpaceId: string, query: Record<string, string>) => {
+    const preparedQuery: FilterQuery<Record_> = {
+        recordSpace: recordSpaceId,
+    }
+
+    if (query.id) {
+        if (!mongoose.Types.ObjectId.isValid(query.id)) {
+            throwBadRequest(`Query field Id: ${query.id} is not a valid ObjectId`);
+        }
+        preparedQuery._id = query.id;
+        delete query.id;
+    }
+
+    const queryKeys = Object.keys(query);
+    if (queryKeys.length) {
+        preparedQuery.$and = [];
+    }
+
+    return { queryKeys, preparedQuery }
 }
