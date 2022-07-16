d67af1e497a4e4322ed6033371da8538fb3bf986
Akintunde Jegede <akintunde.jegede@primarybid.com>
2022-07-16 06:56:44 +0100

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-echo "Setting up Mongo"
-cd docker
-mongo_container="n_mongo"
-mongo_image="mongo"
-if docker ps | awk -v app="$mongo_container" 'NR > 1 && $NF == app{ret=1; exit} END{exit !ret}'; then
-  docker stop "$mongo_container" && docker rm -f "$mongo_container"
-  echo "Deleting old Mongo container"
+echo ">>> *Setting up Mongo"
+mongo_container_name="nc_mongo"
+mongo_image_name="ni_mongo"
+
+if docker ps | awk -v app="$mongo_container_name" 'NR > 1 && $NF == app{ret=1; exit} END{exit !ret}'; then
+   echo ">>> Image Already Exists"  
+   docker stop "$mongo_container_name"  2>&1 >/dev/null
+   echo ">>> Stopped Container"
+   docker rm -f "$mongo_container_name" 2>&1 >/dev/null
+   echo ">>> Deleted Container"
+else
+  echo ">>> Mongo Container not found"
+  echo ">>> Entering Project Docker folder to create new Mongo Image"
+  cd "docker"
+  echo ">>> Creating new Mongo Image"
+  if docker build -f mongo.dockerfile -t "$mongo_image_name" .; then
+    echo ">>> Successfully created new Mongo Image"
+     cd ".."
+  else
+    echo "Docker Build Failed"
+    exit 1
+  fi
+  echo ">>> Exiting Project Docker folder"
 fi
-docker run --name "$mongo_container" -d -p 27017:27017 --env-file ../env/.local.env mongo "$mongo_image"
-cd ..
+echo ">>> Running New Container based on Image"
+newContainer=$(docker run --name "$mongo_container_name" -d -p 27017:27017 --env-file env/.local.env "$mongo_image_name")
+echo ">>> New Container Name: $mongo_container_name"
