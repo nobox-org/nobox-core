3afe766ff686efa2d051f3ce6cae7cbd56cc55f2
Akintunde Jegede <akintunde.jegede@primarybid.com>
2022-07-24 23:52:53 +0100

    test: updated app.controller.spec

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,30 @@
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 describe('AppController', () => {
-  let appController: AppController;
+  let appController: AppController, appService: AppService;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot({ load: [() => ({ serverConfig: { serverName: "test server" } })], })],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    appService = app.get<AppService>(AppService);
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    const mockServerMessage = { hi: "hi", knowMore: "/docs" };
+    jest.spyOn(AppService.prototype, "getHello").mockImplementation(() => mockServerMessage);
+
+    it(`should return server message"} `, () => {
+      expect(appController.getHello()).toBe(mockServerMessage);
     });
+
   });
 });
+
