f40ca229dee27cdd2edbfcd756a1600ec956327b
Akintunde Jegede <jegedeakintunde@gmail.com>
2023-01-29 23:11:13 +0100

    updated env and db setup

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -1,7 +1,11 @@
 import { MONGO_PASSWORD, MONGO_USERNAME, MONGO_HOST, MONGO_PROTOCOL, MONGO_DB_NAME, MONGO_PORT } from "./mainConfig";
 import { NonEmptyArray } from 'src/types';
 
-export const connString = `${MONGO_PROTOCOL}://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}${MONGO_PORT ? ':' + MONGO_PORT : ''}/${MONGO_DB_NAME}?retryWrites=true&w=majority`;
+const authSection = MONGO_USERNAME && MONGO_PASSWORD ? `${MONGO_USERNAME}:${MONGO_PASSWORD}@` : '';
+
+export const isAuth = !!authSection;
+
+export const connString = `${MONGO_PROTOCOL}://${authSection}${MONGO_HOST}${MONGO_PORT ? ':' + MONGO_PORT : ''}/${MONGO_DB_NAME}?retryWrites=true&w=majority`;
 
 export const connOptions: Record<string, boolean> = {
     useUnifiedTopology: true,
