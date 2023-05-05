9351f2de2b00064b2f30f5b873e0078221ec426a
Akintunde Jegede <akintunde.jegede@primarybid.com>
2023-05-05 08:41:53 +0100

    updated auth flow

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express';
 import { ApiOperation } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 
-@Controller("auth")
+@Controller("auth/_/")
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
