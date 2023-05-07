6ad9ba3423f9460ac9dd99fa67577cb8c69b8b8f
Akintunde Jegede <akintunde.jegede@primarybid.com>
2023-05-07 07:53:08 +0100

    fixed logger

--- src/logger/logic.ts
+++ src/logger/logic.ts
@@ -1,143 +1,35 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import * as os from 'os';
+import { LOG_STORE_NAME, init } from './constants';
+import { ILog } from './type';
+import { readFile } from './utils/read-file';
+import { writeFile } from './utils/write-file';
 
-const moduleName = 'ProjectLog';
-
-const initDate = Date.now();
-
-const saveInFile = true;
-
-interface ILog {
-  date: number;
-  data: string;
-  action: any;
-  traceId: string;
-}
-
-
-
-const wrappedLog = (...args: any[]) => {
-  console.log(...args);
-};
-
-const logPool: ILog[] = [];
-
-const logStoreName = path.resolve(__dirname, './logstore.json');
-
-let $reading = false;
-let $writing = false;
-
-export function logExecute(item: ILog) {
+export function logExecute(log: ILog) {
+  const { saveInFile } = init;
 
   if (saveInFile) {
-    logPool.push(item);
-    storeLog();
+    storeLog(log);
   }
 }
 
-async function storeLog() {
-  const index = 0;
-  while (logPool.length > 0) {
-    const log = logPool[index];
-    if (!$reading && !$writing) {
-      // Read
-      const read = await readFile(logStoreName, 'utf8');
-      // Update
-      const logStore = JSON.parse(read || '[{}]'); // Parse
-      logStore.push(log);
-      const stringifiedJSON = JSON.stringify(logStore);
+export async function storeLog(log: ILog) {
+  const { $reading, $writing } = init;
+  if ($reading && $writing) {
+    // Read
+    const read = await readFile(LOG_STORE_NAME, 'utf8', $reading);
 
-      // write and Recall StoreLog() when Done;
-      writeFile(logStoreName, stringifiedJSON, storeLog);
+    // Update
+    const logStore = JSON.parse(read || '[{}]'); // Parse
 
-      logPool.shift(); // remove from Pool
-      continue; // Jump To Next
-    }
-    break; // Break While Loop to Avoid Unnecessary Looping
+    console.log({ logStore, log })
+
+    logStore.push(log);
+
+    console.log({ logStore })
+
+    const stringifiedJSON = JSON.stringify(logStore);
+
+    // write and Recall StoreLog() when Done;
+    writeFile(LOG_STORE_NAME, stringifiedJSON, storeLog, $writing);
   }
 }
 
-export function parseTime(unixTimestamp) {
-  const date = new Date(unixTimestamp);
-  const hours = date.getHours();
-  const minutes = '0' + date.getMinutes();
-  const seconds = '0' + date.getSeconds();
-  const milliseconds = date.getMilliseconds();
-  const formattedTime =
-    hours +
-    ':' +
-    minutes.substr(-2) +
-    ':' +
-    seconds.substr(-2) +
-    ' ' +
-    milliseconds;
-  return formattedTime;
-}
-
-
-function initLogStore() {
-  if (!saveInFile) {
-    return;
-  }
-
-  const fileName = logStoreName;
-  try {
-    if (fs.existsSync(logStoreName)) {
-      return;
-    }
-    // If Store is Not Created yet
-    wrappedLog(`${moduleName}: create log Storage, location: ${logStoreName} `);
-    const logStore = [];
-    logStore.push({
-      date: initDate,
-      value: 'firstLogEver',
-      tag: 'Some Tag',
-    });
-    const stringedJSON = JSON.stringify(logStore);
-    fs.writeFile(fileName, stringedJSON, function (
-      err: NodeJS.ErrnoException | null,
-    ) {
-      if (err) {
-        return wrappedLog('Log Module Error:', err);
-      }
-
-      wrappedLog('Log Store Successfully Created');
-    });
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-
-const readFile = (fileName: string, encoding: any): any =>
-  new Promise((resolve, reject) => {
-    $reading = true;
-    fs.readFile(fileName, encoding, function (err: NodeJS.ErrnoException, data: string) {
-      $reading = false;
-      if (err) {
-        reject(err);
-      }
-      resolve(data as string);
-    });
-  });
-
-const writeFile = (
-  fileName: string,
-  fileToWrite: string,
-  postWrite: () => any,
-) =>
-  new Promise((resolve, reject) => {
-    $writing = true;
-    fs.writeFile(fileName, fileToWrite, err => {
-      $writing = false;
-      postWrite();
-
-      if (err) {
-        reject(err);
-      }
-      resolve(true);
-    });
-  });
-
-export { initLogStore };
---
+++ src/logger/utils/read-file.ts
@@ -0,0 +1,13 @@
+import * as fs from 'fs';
+
+export const readFile = (fileName: string, encoding: any, $reading: boolean): any =>
+    new Promise((resolve, reject) => {
+        $reading = true;
+        fs.readFile(fileName, encoding, function (err: NodeJS.ErrnoException, data: string) {
+            $reading = false;
+            if (err) {
+                reject(err);
+            }
+            resolve(data as string);
+        });
+    });
