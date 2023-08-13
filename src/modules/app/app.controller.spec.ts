87924c3d2f72cff8149851b507e6116b1c7470e0
Akintunde Jegede <jegedeakintunde@gmail.com>
2023-08-13 07:20:45 +0100

    take note of casing

---
+++ src/modules/app/app.controller.spec.ts
@@ -0,0 +1,33 @@
+import { ConfigModule } from '@nestjs/config';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+   let appController: AppController;
+
+   beforeEach(async () => {
+      const app: TestingModule = await Test.createTestingModule({
+         imports: [
+            ConfigModule.forRoot({
+               load: [() => ({ serverConfig: { serverName: 'test server' } })],
+            }),
+         ],
+         controllers: [AppController],
+         providers: [AppService],
+      }).compile();
+
+      appController = app.get<AppController>(AppController);
+   });
+
+   describe('root', () => {
+      const mockServerMessage = { hi: 'hi', knowMore: '/docs' };
+      jest
+         .spyOn(AppService.prototype, 'getHello')
+         .mockImplementation(() => mockServerMessage);
+
+      it(`should return server message"} `, () => {
+         expect(appController.getHello()).toBe(mockServerMessage);
+      });
+   });
+});
