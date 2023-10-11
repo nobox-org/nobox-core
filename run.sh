c5f4da251a14f88c6786b8a2c2e2cd4bd6ca87d4
Akintunde Jegede <akintunde.jegede@primarybid.com>
2023-10-11 08:09:10 +0100

    updated run.sh,reverted db lib

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 runServerPort(){
+    
+    npm install
+
+    npm i @node-rs/argon2-linux-x64-gnu
+
+    pm2 delete nobox-core-8001 nobox-core-8002 nobox-core-8003 nobox-core-8004
+
     SERVER_PORT=8001 pm2 start 'NODE_ENV=dev npm run deploy' --name nobox-core-8001
     SERVER_PORT=8002 pm2 start 'NODE_ENV=dev npm run deploy' --name nobox-core-8002
     SERVER_PORT=8003 pm2 start 'NODE_ENV=dev npm run deploy' --name nobox-core-8003
